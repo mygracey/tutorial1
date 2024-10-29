e420e0eb6f6efa9d4dcd42b9cc025210686a00d2
grace <graceogwe@gmail.com>
2024-10-29 10:38:12 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App(){
+
+  return (
+    <div className="app">
+      <h1>This is a React Tutorial Class</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+         Corrupti atque porro consequuntur vitae culpa sequi saepe
+          officia eius soluta non? Odio, nemo culpa
+         expedita magnam possimus pariatur exercitationem esse unde?</p>
+
+         <button>Learn more</button>
+    </div>
+  )
+
+}
+export default App
